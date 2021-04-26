89c200c4ddbffcbb176cf0c0e19321996a27d76b
ni-shi-ko <72636476+ni-shi-ko@users.noreply.github.com>
2021-04-26 17:44:28 +0900

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,19 @@
 const buttons = document.querySelectorAll('.button');
 const result = document.getElementById('result');
-
-
+const clear = document.getElementById('C');
+const equal = document.getElementById('equal');
+const plus = document.getElementById('plus');
+ 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
-    result.innerHTML = button.value;
+    result.innerHTML += button.value;
 
   })
-})
+});
+
+
+
+clear.addEventListener('click', () => {
+  result.innerHTML = "";
+});
+
