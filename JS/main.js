2dd5f8b92e00af583e4dbf3e79955179b38fc313
ni-shi-ko <72636476+ni-shi-ko@users.noreply.github.com>
2021-04-30 19:54:29 +0900

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,23 +1,36 @@
 const buttons = document.querySelectorAll('.button'); //関数を呼出すコード
 const result = document.getElementById('result');
 const clear = document.getElementById('C');
+const calc = document.getElementsByClassName('.calc');
 
 const addResult = (str) => {
   result.innerHTML += str;
 }
+ 
 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
-    if (result.innerHTML == "0" && button.value != "0") {
+    if (result.innerHTML == "0" && button.value != "." && !button.classList.contains('calc')) {
       result.innerHTML = "";
     }
-    addResult(button.value);
-      //if文 0の場合・・・
-   
-  })
-});
 
+    if (button.value == ".") {
+      
+    if (result.innerHTML.indexOf(".") != -1) {
+        return
+      }
+    } 
+    
+    const arry = result.innerHTML;
+    const Last = arry[arry.length - 1];
+    if (Last == button.classList.contains('calc') ) {
+      
+    }
+    addResult(button.value);
+  })
+}); 
 
 clear.addEventListener('click', () => {
   result.innerHTML = "0";
-});
+}); 
+
