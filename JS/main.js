3d67cd3437d95dd87795d5008ab6df78598cc4ec
ni-shi-ko <72636476+ni-shi-ko@users.noreply.github.com>
2021-04-30 14:40:28 +0900

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,19 +1,23 @@
-const buttons = document.querySelectorAll('.button');
+const buttons = document.querySelectorAll('.button'); //関数を呼出すコード
 const result = document.getElementById('result');
 const clear = document.getElementById('C');
-const equal = document.getElementById('equal');
-const plus = document.getElementById('plus');
- 
+
+const addResult = (str) => {
+  result.innerHTML += str;
+}
+
 buttons.forEach(button => {
   button.addEventListener('click', () => {
-    result.innerHTML += button.value;
-
+    if (result.innerHTML == "0" && button.value != "0") {
+      result.innerHTML = "";
+    }
+    addResult(button.value);
+      //if文 0の場合・・・
+   
   })
 });
-
 
 
 clear.addEventListener('click', () => {
   result.innerHTML = "0";
 });
-
