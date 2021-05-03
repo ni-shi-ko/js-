7442979463a089678d3d205835e488c49647f1d5
ni-shi-ko <72636476+ni-shi-ko@users.noreply.github.com>
2021-05-03 17:33:55 +0900

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -2,34 +2,38 @@ const buttons = document.querySelectorAll('.button'); //関数を呼出すコー
 const result = document.getElementById('result');
 const clear = document.getElementById('C');
 
+
+
 const addResult = (str) => {
   result.innerHTML += str;
 }
  
-
+ 
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     if (result.innerHTML == "0" && button.value != "." && !button.classList.contains('calc')) {
-      result.innerHTML = "";
+      result.innerHTML = ""; 
     }
-
+    
     if (button.value == ".") {
       
     if (result.innerHTML.indexOf(".") != -1) {
         return
       }
     } 
-    
-    const calcList =  ['+', '-', '*', '/'] 
-    const Last = calcList.find(( value  => value == button.value));
-    if (result.innerHTML.lastIndexOf(Last) != -1) {
-      return
+    //最後が演算子の時の処理
+    if (button.classList.contains('calc')) {
+      let Last = result.innerHTML.slice(result.innerHTML.length - 1);
+      if (Last == "+" || Last == "-" || Last == "*" || Last == "/") {
+        return
+      }
     }
+
+  
       addResult(button.value);
   })
 }); 
 
 clear.addEventListener('click', () => {
   result.innerHTML = "0";
-}); 
-
+});
