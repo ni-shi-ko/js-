c132b7df2e112c9ec2e799c4169545ef0f76310f
ni-shi-ko <72636476+ni-shi-ko@users.noreply.github.com>
2021-05-18 22:48:24 +0900

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,36 +1,34 @@
 const buttons = document.querySelectorAll('.button'); //関数を呼出すコード
 const result = document.getElementById('result');
 const clear = document.getElementById('C');
+const Equal = document.getElementById('equal');
+
 
 const addResult = (str) => {
   result.innerHTML += str;
 }
+ 
  //どうしたら数値として判断できるか、どう取得するか、sliceをつかう //
 buttons.forEach(button => {
   button.addEventListener('click', () => {
     if (button.value == "=") {
-      //演算子がなくなるまで繰り返すという処理を作る
-       const plus = "+"
+      //演算子がなくなるまで繰り返すという処理を作る 
+      const Plus = "+"
       let inputText = result.innerHTML;
-      const Plusindex1 = inputText.indexOf(plus)
-      if (Plusindex1 != -1) {
-        let num1 = inputText.slice(0, Plusindex1);
-        alert(num1)
-        const Plusindex2 = inputText.indexOf(plus, (Plusindex1 + 1))
-        let num2 = inputText.slice(Plusindex1 + 1, Plusindex2);
-        alert(num2)
-        let num3 = inputText.slice(Plusindex2 + 1);
-        alert(num3)
-        const Num1 = Number(num1);
-        const Num2 = Number(num2);
-        const Num3 = Number(num3)
-        result.innerHTML = Num1 + Num2 + Num3;
-        return
+      let Nums = [];
+      while (inputText.indexOf(Plus) != -1) {
+        const Plusindex = inputText.indexOf(Plus);
+        Nums.push(inputText.slice(0, Plusindex));//この処理でindex0fで取得した＋の位置より前の数字をpushしている
+        inputText = inputText.slice(Plusindex + 1);//１つ目の＋以降の式をまたinputTextに代入してまた＋の位置を探して数字を取得している
+        
       }
+      console.log(Nums)
+      return
     }
     if (result.innerHTML == "0" && button.value != "." && !button.classList.contains('calc')) {
       result.innerHTML = ""; // == は関数左右が同じだったらtrueを返す
     }
+    
     if (button.value == ".") {
     if (result.innerHTML.indexOf(".") != -1) {
         return
@@ -43,9 +41,11 @@ buttons.forEach(button => {
         return
       }
     }
+   
       addResult(button.value);
   })
 }); 
+
 
 clear.addEventListener('click', () => {
   result.innerHTML = "0";
